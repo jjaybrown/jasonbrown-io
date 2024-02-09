2fa5215b3ff0de02022da993552b77aab8db42ad
Jason Brown <jason.brown.delta@gmail.com>
2024-02-09 12:23:54 +0000

    init repo

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "Jason Brown";
+export const SITE_DESCRIPTION = "Engineering leader, aviator, innovator";
+export const TWITTER_HANDLE = "@jjaybrown";
+export const MY_NAME = "Jason Brown";
+
+// setup in astro.config.mjs
+const BASE_URL = new URL(import.meta.env.SITE);
+export const SITE_URL = BASE_URL.origin;
